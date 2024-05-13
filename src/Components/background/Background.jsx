233ab410ed0c3f3f766bf8a52c55b1f3f9195e2f
Morita <98917348+lunajulio@users.noreply.github.com>
2024-05-13 00:46:03 -0500

    Resuelve conflictos de fusión

--- src/Components/background/Background.jsx
+++ src/Components/background/Background.jsx
@@ -1,12 +1,35 @@
-
 // Background.jsx
-import fondo from '../../assets/fondo.png';
+import { useEffect, useState } from 'react';
 import './background.css';
+import imagenSuperpuesta from '../../assets/header-img.svg';
 
 const Background = () => {
+  const [text, setText] = useState('');
+  const fullText = "HELLO WORLD!";
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      setText(fullText.substring(0, currentIndex + 1));
+      currentIndex++;
+      if (currentIndex === fullText.length) {
+        clearInterval(interval);
+      }
+    }, 200); // Velocidad de escritura (en milisegundos)
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="background" style={{ backgroundImage: `url(${fondo})` }}>
-      {/* Aquí puedes colocar cualquier contenido adicional que desees */}
+    <div className="background-container">
+      <div className="background-image"></div>
+      <div className="background-content">
+        <img src={imagenSuperpuesta} alt="Imagen Superpuesta" className="imagen-superpuesta" />
+        <div>
+          <h1 className="background__title">{text}</h1>
+          <p className="descrip">Design and create with UML the fun way! Discover our interactive tools to learn and play while you bring your ideas to life.</p>
+        </div>
+      </div>
+      <button className="background__button">CREATE</button>
     </div>
   );
 };
--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,18 +1,17 @@
 // Navbar.jsx
 import './navbar.css';
-import jupiter from '../../assets/jupiter.png'; // Ruta a tu imagen de icono
+
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar__logo">
-        <img src={jupiter} alt="Logo Icon" className="navbar__logo-icon" />
-        JUPITER
+                JUPITER
       </div>
       <ul className="navbar__menu">
-        <li className="navbar__menu-item">APRENDER</li>
-        <li className="navbar__menu-item">JUGAR</li>
-        <li className="navbar__menu-item">CREAR</li>
+        <li className="navbar__menu-item">CONCEPTS</li>
+        <li className="navbar__menu-item">CHALLENGE</li>
+        <li className="navbar__menu-item">CREATE</li>
       </ul>
     </div>
   );
